872d01fc8e462e0a0760df1d7ed861c939c74f8c
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-12 20:40:25 -0500

    feat(Program): inital driver class

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper;
+
+namespace w1_basic_file_i_o_phenrichsbowe;
+
+public class Program
+{
+    public static void Main()
+    {
+        new MenuController();
+
+        //Character character = new();
+        //Console.WriteLine($"Welcome, {character.Name} the {character.Class}! You are level {character.Level} and your equipment includes: {string.Join(", ", character.Equipment)}.");
+    }
+}
